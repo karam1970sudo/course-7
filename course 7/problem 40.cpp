5c108abf69f50e0478a624e253417ca729e1257a
karam <karamha1244@gmail.com>
2026-07-08 18:49:09 +0300

    end

--- course 7/problem 40.cpp
+++ course 7/problem 40.cpp
@@ -13,10 +13,23 @@ string JoinString(vector <string> vStr, string dm)
 	return S1.substr(0, S1.length() - dm.length());
 }
 
+string JoinString(string arr[100], short length, string dm)
+{
+	string S1 = "";
+	for (short i = 0; i < length; i++)
+	{
+		S1 = S1 + arr[i] + dm;
+	}
+	return S1.substr(0, S1.length() + dm.length());
+}
+
 int main()
 {
+	string arr[] = { "Mohammed","Faid","Ali","Maher" };
 	vector<string> vString = { "Mohammed","Faid","Ali","Maher" };
 	cout << "\nVector after join: \n";
-	cout << JoinString(vString, "###");
+	cout << JoinString(vString, " ");
+	cout << "\nArry after join: \n";
+	cout << JoinString(arr,4," ");
 	system("pause>0");
 }
